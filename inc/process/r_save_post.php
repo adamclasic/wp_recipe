3ba1e343cbeccb1da985a7a6145e97e24c9ccb57
adam allalou <aabou9@gmail.com>
2021-01-11 21:47:07 +0100

    add test if entry exist

--- inc/process/r_save_post.php
+++ inc/process/r_save_post.php
@@ -1,8 +1,9 @@
 <?php
 function r_save_post($post_id, $post, $update)
 {
-  $our_metadata = [];
-  $our_metadata['ratings'] = '0';
-  $our_metadata['count'] = '0';
+  $our_metadata = get_post_meta('recipe_ratings');
+  $our_metadata = empty($our_metadata) ? [] : $our_metadata;
+  $our_metadata['ratings'] = isset($our_metadata['ratings']) ? $our_metadata['ratings'] : '0';
+  $our_metadata['count'] = isset($our_metadata['count']) ? $our_metadata['count'] : '0';
   update_post_meta($post_id, 'recipe_ratings', $our_metadata);
 }
