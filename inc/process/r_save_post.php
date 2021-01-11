3d6d288f7cefb30abce3c8ceec60ba2f57f1240f
adam allalou <aabou9@gmail.com>
2021-01-11 21:39:36 +0100

    fix post meta database issue

--- inc/process/r_save_post.php
+++ inc/process/r_save_post.php
@@ -3,5 +3,6 @@ function r_save_post($post_id, $post, $update)
 {
   $our_metadata = [];
   $our_metadata['ratings'] = '0';
-  update_post_meta($post_id, 'recipe_ratings', absint($our_metadata));
+  $our_metadata['count'] = '0';
+  update_post_meta($post_id, 'recipe_ratings', $our_metadata);
 }
