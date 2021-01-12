b7e7e7c0d5b2f0d036165552e5b1d20151dbecdc
adam allalou <aabou9@gmail.com>
2021-01-12 22:35:31 +0100

    add rating stars template

--- inc/process/filter_content.php
+++ inc/process/filter_content.php
@@ -3,6 +3,10 @@
 function filter_content($the_content)
 {
   global $post;
+  $rate_content = file_get_contents( PN_PLUGIN_ROOT . 'inc/recipe-template.php' );
+  $rate_content = str_replace('RATE_I18N', 'Rating', $rate_content);
+  $rate_content = str_replace('RECIPE_ID', $post->ID, $rate_content);
+  global $post;
   $post_id = $post->ID;
   if(is_singular('recipe')) {
     // var_dump(get_post_meta($post_id, 'recipe_ratings', true));
@@ -10,5 +14,5 @@ function filter_content($the_content)
     $count = get_post_meta($post_id, 'recipe_ratings', true)['count'];
     $html = '<p>test' . $ratings . $count . '</p>';
   }
-  return $the_content . $html;
+  return $the_content . $rate_content;
 }
