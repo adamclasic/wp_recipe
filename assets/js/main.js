86cb8006cd09d2a0d5acf9d4ee49e9c4d3b3790a
adam allalou <aabou9@gmail.com>
2021-01-13 12:09:21 +0100

    add disabled to input

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,13 @@
   console.log('hey');
   let ratingForm = document.querySelector('.rating-form');
   ratingForm.addEventListener("submit", function(event){
-    event.preventDefault()
+    event.preventDefault();
+    $("#rating-number").attr("disabled", true);
     let $inputVal = document.querySelector('#rating-number').value;
     let ratingVal = {'rVal': $inputVal,
                     'action': 'r_rating_form'};
     // ratingVal
-    // console.log( document.querySelector('#rating-number').value);
+    console.log( this.firstChild );
     $.post(recipe_obj.ajax_url, ratingVal, (data) => {console.log(data)});
     // console.log();
 });
