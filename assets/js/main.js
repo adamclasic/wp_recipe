d7cef94cab02aaaaed3684c3dd8fa3d4a342dd7f
adam allalou <aabou9@gmail.com>
2021-01-13 11:24:51 +0100

    fix event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 (function($){
   console.log('hey');
-
-let ratingVal = {rVal: document.querySelector('#rating-number').value};
-let ratingForm = document.querySelector('.rating-form');
-ratingForm.addEventListener("submit", function(event){
-  event.preventDefault()
-  
-  // ratingVal
-  console.log(recipe_obj.ajax_url);
-  $.post(recipe_obj.ajax_url, {'action': 'r_rating_form', 'ratingVal': 'USA'}, (data) => {console.log(data)})
-  // console.log();
+  let ratingForm = document.querySelector('.rating-form');
+  ratingForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    let $inputVal = document.querySelector('#rating-number').value;
+    let ratingVal = {'rVal': $inputVal,
+                    'action': 'r_rating_form'};
+    // ratingVal
+    // console.log( document.querySelector('#rating-number').value);
+    $.post(recipe_obj.ajax_url, ratingVal, (data) => {console.log(data)});
+    // console.log();
 });
 })(jQuery);
