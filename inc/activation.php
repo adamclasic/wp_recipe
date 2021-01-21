cffcccfc7ece17b1f9e623ecf9d30d23edd33872
adam allalou <aabou9@gmail.com>
2021-01-21 15:02:28 +0100

    add prepare to sql queries

--- inc/activation.php
+++ inc/activation.php
@@ -4,7 +4,7 @@ function r_activate()
 {
   global $wpdb;
   $the_var = $wpdb->prefix;
-  $createSQL = "CREATE TABLE `wrdprs2`.`" . $the_var . "_ratings` ( `ID` BIGINT(20) NOT NULL , `recipe_id` BIGINT(20) NOT NULL , `user_ip` TEXT NOT NULL , `rating` FLOAT(3.2) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;";
+  $createSQL = $wpdb->prepare("CREATE TABLE `wrdprs2`.`%s_ratings` ( `ID` BIGINT(20) NOT NULL , `recipe_id` BIGINT(20) NOT NULL , `user_ip` TEXT NOT NULL , `rating` FLOAT(3.2) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;", [$the_var]);
   require( ABSPATH . "/wp-admin/includes/upgrade.php" );
   dbDelta( $createSQL );
   wp_schedule_event( time(), 'daily', 'my_daily_event' );
