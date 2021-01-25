e40c33e3539be42c5bc57a75d78fd7f98ff8682b
adam allalou <aabou9@gmail.com>
2021-01-25 08:16:59 +0100

    add http api request

--- inc/admin/menu_page.php
+++ inc/admin/menu_page.php
@@ -19,5 +19,10 @@ function wpdocs_register_my_custom_menu_page(){
 * Display a custom menu page
 */
 function my_custom_menu_page(){
+  $r_form_content = wp_remote_get( plugins_url('assets\extras\options-page.txt', RECIPE_PLUGIN_URL) );
+  // if ( is_array( $r_form_content ) && ! is_wp_error( $r_form_content ) ) {
+    // $body    = $r_form_content['body']; // use the content
+// }
+  echo($r_form_content['body']);
   echo 'Admin Page Test';
 }
