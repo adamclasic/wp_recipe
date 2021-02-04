c22aea352d509a67a382fed169f7bfc03527c16c
adam allalou <aabou9@gmail.com>
2021-02-04 11:03:39 +0100

    add blockControl

--- blocks/app/recipe_blocks/index.js
+++ blocks/app/recipe_blocks/index.js
@@ -5,7 +5,7 @@ const { registerBlockType } = wp.blocks
 
 const { PanelBody, PanelRow, TextControl, SelectControl } = wp.components;
 const { withState } = wp.compose;
-const { InspectorControls } = wp.editor;
+const { InspectorControls, AlignmentToolbar, BlockControls } = wp.editor;
 
 const MySelectControl = withState( {
     size: '50%',
@@ -31,6 +31,9 @@ registerBlockType( 'reccpe/ings', {
             source: 'text',
             // default: 'my dog'
           },
+    textSide: {
+      source: 'text'
+    }
   },
   edit: ( props ) => {
     return [
@@ -40,7 +43,7 @@ registerBlockType( 'reccpe/ings', {
           <PanelRow>
             <p>give a title to the recipe.</p>
           </PanelRow>
-          <TextControl 
+          <TextControl
             value= {props.attributes.name}
             help='ex: French Fris'
             onChange= {(newVal) => {
@@ -52,14 +55,25 @@ registerBlockType( 'reccpe/ings', {
 
       </InspectorControls>,
     <div>
-      <p>name: {props.attributes.name}</p>
-      <p>dificulty: </p>
+      <BlockControls>
+        <AlignmentToolbar
+        value={props.attributes.textSide}
+        onChange={(newVall) => {
+          props.setAttributes({textSide: newVall})
+        }}></AlignmentToolbar>
+      </BlockControls>
+      <p style={{textAlign: props.attributes.textSide}}>
+      name: {props.attributes.name}</p>
+      <p style={{color: 'red'}}>dificulty: </p>
     </div>
     ]
   },
   
   save: ( props ) => {
-    return <p>hi im save</p>
+    return <div>
+              <p>name: {props.attributes.name}</p>
+              <p>dificulty: </p>
+            </div>
   },
   support: {
     html: false,
